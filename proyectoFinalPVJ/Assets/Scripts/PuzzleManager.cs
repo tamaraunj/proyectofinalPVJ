11b54339b4371178be79685b12aea8e7e7ad2684
tamara m <45763997@fi.unju.edu.ar>
2025-11-22 03:48:58 -0300

    Logica para que al resolver el puzzle de interruptores se prenda una luz verde

--- proyectoFinalPVJ/Assets/Scripts/PuzzleManager.cs
+++ proyectoFinalPVJ/Assets/Scripts/PuzzleManager.cs
@@ -4,9 +4,18 @@ public class PuzzleManager : MonoBehaviour
 {
     private int[] patronCorrecto = { 2, 0, 1, 3 };
     private int indice = 0;
-
+    [SerializeField] private Light luz;
+    [SerializeField] private Renderer esfera;
+    [SerializeField] private Material materialVerde;
+    private bool puzzleResuelto = false;
     public void RecibirInput(int idInterruptor)
     {
+        if (puzzleResuelto)
+        {
+            Debug.Log("Puzzle TERMINADO");
+            return;
+        }
+
         if (idInterruptor == patronCorrecto[indice])
         {
             Debug.Log("Correcto: " + idInterruptor);
@@ -14,8 +23,8 @@ public class PuzzleManager : MonoBehaviour
 
             if(indice >= patronCorrecto.Length)
             {
-                Debug.Log("Puzzle CORRECTO");
-                indice = 0;
+                ResolverPuzzle();
+
             }
         }
         else
@@ -23,7 +32,23 @@ public class PuzzleManager : MonoBehaviour
             /* Reiniciar si el jugador se equivoca*/
             Debug.Log("Incorrecto. Reiniciando...");
             indice = 0;
+            
         }
     }
+    public void ResolverPuzzle()
+    {
+        Debug.Log("Puzzle CORRECTO");
+        indice = 0;
+        puzzleResuelto = true;    
 
+        if (luz != null)
+        {
+            luz.enabled = true;
+        }
+        if (esfera != null && materialVerde != null)
+        {
+            esfera.material = materialVerde;
+        }
+            
+    }
 }
