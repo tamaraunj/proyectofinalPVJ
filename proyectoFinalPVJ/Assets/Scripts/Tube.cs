9b38f866af78cb2a57fdd694e7989716f41da003
tamara m <45763997@fi.unju.edu.ar>
2025-11-24 03:16:30 -0300

    Se implementa logica para agregar liquido a los tubos
    Es un prototipo para agregar liquido a los tubos

--- proyectoFinalPVJ/Assets/Scripts/Tube.cs
+++ proyectoFinalPVJ/Assets/Scripts/Tube.cs
@@ -1,11 +1,10 @@
 using UnityEngine;
-using UnityEngine.Rendering;
-
 public class Tube : MonoBehaviour
 {
     public float capacidadMaxima = 100f;
     public float cantidadActual = 0f;
-    
+    public Transform liquidoVisual;
+
     public float AgregarLiquido(float cantidad)
     {
         float espacioDisponible = capacidadMaxima - cantidadActual;
@@ -14,10 +13,25 @@ public class Tube : MonoBehaviour
         return cantidadActual;
     }
 
-    public float EliminarCodigo(float cantidad)
+    public float EliminarLiquido(float cantidad)
     {
         float cantidadEliminada =  Mathf.Min(cantidad, cantidadActual); 
         cantidadActual -= cantidadEliminada;
         return cantidadEliminada;
+    }
+    void Update()
+    {
+        ActualizarVisual();
+
+    }
+    void ActualizarVisual()
+    {
+        if (liquidoVisual == null) return;
+        float porcentaje = cantidadActual/capacidadMaxima;
+        liquidoVisual.localScale = new Vector3(liquidoVisual.localScale.x, porcentaje,liquidoVisual.localScale.z);
+
+        liquidoVisual.localPosition = new Vector3(0,porcentaje / 2f,0);
+
+        liquidoVisual.gameObject.SetActive(porcentaje > 0f);
     }
 }
--- proyectoFinalPVJ/Assets/Scripts/TubeManager.cs
+++ proyectoFinalPVJ/Assets/Scripts/TubeManager.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 public class TubeManager : MonoBehaviour
 {
     public Transform[] posiciones;
-    public Transform[] tubos;
+    public Tube[] tubos;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         for (int i = 0; i < tubos.Length; i++)
         {
-            tubos[i].position = posiciones[i].position;
+            tubos[i].transform.position = posiciones[i].position;
         }
     }
     // Update is called once per frame
@@ -24,17 +24,21 @@ public class TubeManager : MonoBehaviour
         {
             Intercambiar(1, 2);
         }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            AgregarLiquido();
+        }
 
     }
     public void Intercambiar(int i, int j)
     {
 
-        Transform temp = tubos[i];
+        Tube temp = tubos[i];
         tubos[i] = tubos[j];
         tubos[j] = temp;
 
-        tubos[i].position = posiciones[i].position;
-        tubos[j].position = posiciones[j].position;
+        tubos[i].transform.position = posiciones[i].position;
+        tubos[j].transform.position = posiciones[j].position;
     }
 
     public void IntercambiarMedio()
@@ -45,5 +49,15 @@ public class TubeManager : MonoBehaviour
     public void IntercambiarDerecha()
     {
         Intercambiar(1, 2);
+    }
+
+    public void AgregarLiquido()
+    {
+        Tube tuboDestino = tubos[0];
+        Tube tuboOrigen = tubos[1];
+
+        float cantidadAEliminar = tuboOrigen.cantidadActual;
+        float retirada = tuboOrigen.EliminarLiquido(cantidadAEliminar);
+        tuboDestino.AgregarLiquido(retirada);
     }
 }
