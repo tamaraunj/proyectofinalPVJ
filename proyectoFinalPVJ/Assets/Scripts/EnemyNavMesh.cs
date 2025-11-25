81c8b0c82e49dd716d1d89c181521fe0e7768c75
tamara m <45763997@fi.unju.edu.ar>
2025-11-25 16:08:30 -0300

    Enemigo: persigue al jugador segun una distancia

--- proyectoFinalPVJ/Assets/Scripts/EnemyNavMesh.cs
+++ proyectoFinalPVJ/Assets/Scripts/EnemyNavMesh.cs
@@ -3,18 +3,38 @@ using UnityEngine.AI;
 
 public class EnemyNavMesh : MonoBehaviour
 {
-    NavMeshAgent agente;
+    public NavMeshAgent enemigo;
     [SerializeField] private Transform jugador;
+    [SerializeField] private float velocidadEnemigo;
+    [SerializeField] private float rango;
+    [SerializeField] private float distancia;
+    public bool persiguiendo;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       agente = GetComponent<NavMeshAgent>(); 
+       enemigo = GetComponent<NavMeshAgent>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        agente.destination = jugador.position;
+        distancia = Vector3.Distance(enemigo.transform.position, jugador.position);
+        persiguiendo = distancia <=rango;
+        if(persiguiendo)
+        {
+            enemigo.speed = velocidadEnemigo;
+            enemigo.SetDestination(jugador.position);
+        }
+        else
+        {
+            enemigo.speed = 0;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, rango);
     }
 }
